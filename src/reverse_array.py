0a0dd89a092636b92b37531c2e810abfbaf9599f
mateusaguiiiar <mateusaguiarbf123@gmail.com>
2026-05-07 13:00:17 -0300

    reverse array concluído

--- src/reverse_array.py
+++ src/reverse_array.py
@@ -2,4 +2,13 @@ from src.my_array import MyArray
 
 
 def reverse_array(array: MyArray) -> MyArray:
-    raise NotImplementedError
+    left = 0
+    right = len(array) - 1
+
+    while left < right:
+        array[left], array[right] = array[right], array[left]
+
+        left += 1
+        right -= 1
+
+    return array
