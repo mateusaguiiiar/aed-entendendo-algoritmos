4bdb669cc83ed83848a845f8339a70f4c3688d8a
mateusaguiiiar <mateusaguiarbf123@gmail.com>
2026-05-12 20:50:47 -0300

    has cicle list concluído

--- src/reverse_linked_list.py
+++ src/reverse_linked_list.py
@@ -8,7 +8,6 @@ def reverse_linked_list(head: MyNode) -> MyNode:
     while current is not None:
         next_node = current.next
         current.next = prev
-
         prev = current
         current = next_node
 
--- src/valid_parentheses.py
+++ src/valid_parentheses.py
@@ -3,20 +3,15 @@ from src.my_stack import MyStack
 
 def is_valid_parentheses(string: str) -> bool:
     stack = MyStack()
-
     mapping = {")": "(", "}": "{", "]": "["}
 
     for char in string:
         if char in "({[":
             stack.push(char)
-
         elif char in ")}]":
             if stack.is_empty():
                 return False
-
-            top_element = stack.pop()
-
-            if mapping[char] != top_element:
+            if stack.pop() != mapping[char]:
                 return False
 
     return stack.is_empty()
