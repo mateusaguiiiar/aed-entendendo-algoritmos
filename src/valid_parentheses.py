8844f2fc5b01daf3f83d0ddf7c68a632ab24026a
mateusaguiiiar <mateusaguiarbf123@gmail.com>
2026-05-11 22:29:32 -0300

    valid parentheses concluído

--- src/valid_parentheses.py
+++ src/valid_parentheses.py
@@ -1,5 +1,22 @@
-# from src.my_stack import MyStack
+from src.my_stack import MyStack
 
 
 def is_valid_parentheses(string: str) -> bool:
-    raise NotImplementedError
+    stack = MyStack()
+
+    mapping = {")": "(", "}": "{", "]": "["}
+
+    for char in string:
+        if char in "({[":
+            stack.push(char)
+
+        elif char in ")}]":
+            if stack.is_empty():
+                return False
+
+            top_element = stack.pop()
+
+            if mapping[char] != top_element:
+                return False
+
+    return stack.is_empty()
