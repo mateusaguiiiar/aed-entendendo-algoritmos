a632dce343cba651e8886984bfc32867a8b243e5
mateusaguiiiar <mateusaguiarbf123@gmail.com>
2026-05-12 21:28:34 -0300

    moonwake concluído

--- src/kth_to_last.py
+++ src/kth_to_last.py
@@ -2,4 +2,21 @@ from src.my_node import MyNode
 
 
 def kth_to_last(head: MyNode, k: int) -> int:
-    raise NotImplementedError
+    slow = head
+    fast = head
+    if k <= 0:
+        return -1
+
+    for i in range(k):
+        if fast is None:
+            return -1
+        fast = fast.next
+
+    while fast is not None:
+        fast = fast.next
+        slow = slow.next
+
+    if slow is not None:
+        return slow.value
+
+    return -1
