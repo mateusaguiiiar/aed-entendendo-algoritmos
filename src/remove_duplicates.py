4c5861ce2f6dca4393bb77fbe67e111492af2e28
mateusaguiiiar <mateusaguiarbf123@gmail.com>
2026-05-12 21:03:28 -0300

    remove duplicates concluído

--- src/remove_duplicates.py
+++ src/remove_duplicates.py
@@ -2,4 +2,18 @@ from src.my_node import MyNode
 
 
 def remove_duplicates(head: MyNode) -> MyNode:
-    raise NotImplementedError
+    current = head
+
+    while current is not None:
+        runner = current
+
+        while runner.next is not None:
+            if runner.next.value == current.value:
+                runner.next = runner.next.next
+                pass
+            else:
+                runner = runner.next
+
+        current = current.next
+
+    return head
