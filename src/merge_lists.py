35e5bcc068d5ce7579da367c0b37cc131c233390
mateusaguiiiar <mateusaguiarbf123@gmail.com>
2026-05-12 22:40:35 -0300

    mmerge lists concluído

--- src/merge_lists.py
+++ src/merge_lists.py
@@ -2,4 +2,22 @@ from src.my_node import MyNode
 
 
 def merge_lists(lista1: MyNode, lista2: MyNode) -> MyNode:
-    raise NotImplementedError
+    dummy = MyNode(0)
+    tail = dummy
+
+    while lista1 is not None and lista2 is not None:
+        if lista1.value <= lista2.value:
+            tail.next = lista1
+            lista1 = lista1.next
+        else:
+            tail.next = lista2
+            lista2 = lista2.next
+
+        tail = tail.next
+
+    if lista1 is not None:
+        tail.next = lista1
+    elif lista2 is not None:
+        tail.next = lista2
+
+    return dummy.next
